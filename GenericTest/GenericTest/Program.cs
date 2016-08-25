c462ab10175468c94f9150ae4f916d99b58c97ad
Johnson Ho <m97_jno@hotmail.com>
2016-08-25 10:32:26 +0200

    init

---
+++ GenericTest/GenericTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void NonGenericTest()
+        {
+
+            var list = new ArrayList();
+
+            list.Add(3);
+        }
+    }
+}
