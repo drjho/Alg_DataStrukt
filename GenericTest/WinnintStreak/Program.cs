2ad50e6ccd07ce434f00c4bd7d0d0c3d4b138666
Johnson Ho <joho77@gmail.com>
2016-11-16 00:21:33 +0100

    2nd commit Winning Streak = wrong assumption on permutation.

--- GenericTest/WinnintStreak/Program.cs
+++ GenericTest/WinnintStreak/Program.cs
@@ -13,41 +13,73 @@ namespace WinnintStreak
 
         static void Main(string[] args)
         {
+            // inputs
             var inputs = Console.ReadLine().Split(' ');
             var matches = int.Parse(inputs[0]);
             var winP = float.Parse(inputs[1], CultureInfo.InvariantCulture);
             var p = GetProbs(matches, winP);
             Console.WriteLine(string.Join(" ", p));
+
+            // calculation
             var m = GetProbMatches(matches);
             float res = 0;
             for (int i = 0; i < matches; i++)
             {
-                res += m[i] * p[i] * i;
-                Console.WriteLine(m[i] + " " + p[i] + " " + i);
+                if (i > 1) // when winning more than once
+                {
+                    Console.WriteLine(i + " here");
+                    res += p[i] * i * 2;
+                    Console.WriteLine(2 + " " + p[i] + " " + i);
+
+                    var tmp = m[i] - 2;
+                    Console.WriteLine("tmp: " + tmp);
+                    var limit = Math.Ceiling(tmp / 2f);
+                    for (int j = 0; j < limit; j++)
+                    {
+                        var rst = j + 1;
+                        res += p[i] * rst * 2;
+                        Console.WriteLine(p[i] + " " + rst);
+                    }
+                }
+                else // win once or less
+                {
+                    res += m[i] * p[i] * i;
+                    Console.WriteLine(m[i] + " " + p[i] + " " + i);
+                }
             }
+            res += m[matches] * p[matches] * matches;
+            // output
             Console.WriteLine(res);
         }
 
         static int[] GetProbMatches(int matches)
         {
+            // how many matches with a certain probablity
+            // it is not P(n,k) !!
             var arr = new int[matches + 1];
-            for (int i = matches; i >= arr.Length/2; i--)
+            for (int i = matches; i >= arr.Length / 2; i--)
             {
                 int f = 1;
                 for (int j = matches; j > i; j--)
                 {
                     f *= j;
                 }
-                arr[i] = f;
+                if (i < matches - 1)
+                    f /= 2;
+                arr[i] = f; 
                 arr[matches - i] = f;
             }
-            
+            //if (matches % 2 == 0)
+            //{
+            //    arr[arr.Length / 2] /= 2;
+            //}
             Console.WriteLine(string.Join(" ", arr));
             return arr;
         }
 
         static float[] GetProbs(int matches, float winP)
         {
+            // probabiliy: from all lost to all win
             var probs = new float[matches + 1];
             for (int i = 0; i <= matches; i++)
             {
