88da9123905a8f6d1b05f54da79a31d63122aa1c
Johnson Ho <joho77@gmail.com>
2016-11-18 12:49:34 +0100

    kattis

---
+++ GenericTest/CD/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD
+{
+    class Program
+    {
+        static bool[] cds;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var n = int.Parse(line[0]);
+                var m = int.Parse(line[1]);
+                if (n == 0 && m == 0)
+                    break;
+                cds = new bool[1000000001];
+                for (int i = 0; i < n; i++)
+                {
+                    cds[int.Parse(Console.ReadLine())] = true;
+                }
+                var res = 0;
+                for (int i = 0; i < m; i++)
+                {
+                    if (cds[int.Parse(Console.ReadLine())])
+                    {
+                        res++;
+                    }
+                }
+                Console.WriteLine(res);
+
+            }
+        }
+    }
+}
---
+++ GenericTest/OverlappingMaps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverlappingMaps
+{
+    class Program
+    {
+        // https://en.wikipedia.org/wiki/Transformation_matrix#Affine_transformations
+        // T * R * S
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
