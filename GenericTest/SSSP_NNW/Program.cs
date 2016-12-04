6e0ebbfdef157f90bfc887cdc76d6945c05f63b4
Johnson Ho <joho77@gmail.com>
2016-12-04 20:21:52 +0100

    run time error, need try catch

--- GenericTest/SSSP_NNW/Program.cs
+++ GenericTest/SSSP_NNW/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SSSP_NNW
 {
@@ -12,7 +9,6 @@ namespace SSSP_NNW
         class Node
         {
             public int u;
-            public Node previous;
             public float f;
 
             public Node(int u)
@@ -28,9 +24,9 @@ namespace SSSP_NNW
                 return x.u.Equals(y.u);
             }
 
-            public int GetHashCode(Node obj)
+            public int GetHashCode(Node n)
             {
-                return obj.u.GetHashCode();
+                return n.u.GetHashCode();
             }
         }
 
@@ -42,82 +38,87 @@ namespace SSSP_NNW
             }
         }
 
-        static Node[] nodes;
-
-        static Dictionary<int, Dictionary<int, float>> graph = new Dictionary<int, Dictionary<int, float>>();
+        static Dictionary<int, Dictionary<int, float>> graph;
 
         static string[] lines;
 
         static void Main(string[] args)
         {
-            lines = Console.ReadLine().Split(' ');
-            var numN = int.Parse(lines[0]);
-            var numE = int.Parse(lines[1]);
-            var numQ = int.Parse(lines[2]);
-            var start = int.Parse(lines[3]);
-
-            nodes = new Node[numN];
-            for (int i = 0; i < numN; i++)
+            try
             {
-                graph[i] = new Dictionary<int, float>();
-            }
 
-            for (int i = 0; i < numE; i++)
-            {
-                AddEdge(Console.ReadLine().Split(' '));
-            }
 
-            for (int i = 0; i < numQ; i++)
-            {
-                var goal = int.Parse(Console.ReadLine());
-                if (start == goal)
+                while (true)
                 {
-                    Console.WriteLine(0);
-                    continue;
-                }
-                //var visited = new Dictionary<Node, float>(new EqualComparer());
-                var visited = new Dictionary<int, Node>();
-                var candidates = new SortedDictionary<Node, float>(new Comparer());
-                candidates.Add(new Node(start), 0);
-                while (candidates.Count > 0)
-                {
-                    var current = candidates.First();
-                    candidates.Remove(current.Key);
-                    foreach (var e in graph[current.Key.u])
+                    lines = Console.ReadLine().Split(' ');
+                    var numN = int.Parse(lines[0]);
+                    var numE = int.Parse(lines[1]);
+                    var numQ = int.Parse(lines[2]);
+                    var start = int.Parse(lines[3]);
+
+                    if (numN == 0 && numE == 0 && numQ == 0 && start == 0)
+                        return;
+
+                    graph = new Dictionary<int, Dictionary<int, float>>();
+                    for (int i = 0; i < numN; i++)
                     {
-                        var n = new Node(e.Key) { previous = current.Key };
-                        
-                        if (e.Key == goal)
-                        {
-                            Console.WriteLine(GetPathSteps(n));
-                            break;
-                        }
-                        n.f = current.Key.f + GetWeight(current.Key.u, n.u);
-
-                        if (candidates.ContainsKey(n) && candidates[n] < n.f)
-                            continue;
-                        if (visited.ContainsKey(n.u) && visited[n.u].f < n.f)
-                            continue;
-                        candidates[n] = n.f;
-
+                        graph[i] = new Dictionary<int, float>();
                     }
-                    //visited.Add(current.Key, current.Value);
-                    visited[current.Key.u] = current.Key;
+
+                    for (int i = 0; i < numE; i++)
+                    {
+                        AddEdge(Console.ReadLine().Split(' '));
+                    }
+
+                    for (int i = 0; i < numQ; i++)
+                    {
+                        var goal = int.Parse(Console.ReadLine());
+                        if (start == goal)
+                        {
+                            Console.WriteLine(0);
+                            continue;
+                        }
+
+                        var visited = new Dictionary<int, Node>();
+                        var candidates = new SortedDictionary<Node, float>(new Comparer());
+                        candidates[new Node(start)] = 0;
+                        bool found = false;
+                        while (candidates.Count > 0)
+                        {
+                            var current = candidates.First();
+                            if (current.Key.u == goal)
+                            {
+                                Console.WriteLine(current.Key.f);
+                                found = true;
+                                break;
+                            }
+                            candidates.Remove(current.Key);
+
+                            foreach (var e in graph[current.Key.u])
+                            {
+                                var n = new Node(e.Key);
+
+                                n.f = current.Key.f + e.Value;
+
+                                if (candidates.ContainsKey(n) && candidates[n] < n.f)
+                                    continue;
+                                if (visited.ContainsKey(n.u) && visited[n.u].f < n.f)
+                                    continue;
+                                candidates[n] = n.f;
+
+                            }
+                            visited[current.Key.u] = current.Key;
+                        }
+                        if (!found)
+                            Console.WriteLine("Impossible");
+                    }
                 }
             }
-        }
-
-
-
-        static int GetPathSteps(Node n)
-        {
-            int c = 0;
-            while (n.previous != null)
+            catch (Exception)
             {
-                c++;
-                n = n.previous;
+
+                throw;
             }
-            return c;
         }
 
         static float GetWeight(int u, int v)
